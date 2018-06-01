120cd3e9966921a07eef74a880fe1df20cbea77a
ipman98 <navilk@zoho.com>
2018-06-01 19:47:54 +0530

    root commit

---
+++ MainForm.Designer.vb
@@ -0,0 +1,211 @@
+﻿'
+' Created by SharpDevelop.
+' User: IP-Man
+' Date: 1/9/2017
+' Time: 1:51 PM
+' 
+Partial Class MainForm
+	Inherits System.Windows.Forms.Form
+	
+	''' <summary>
+	''' Designer variable used to keep track of non-visual components.
+	''' </summary>
+	Private components As System.ComponentModel.IContainer
+	
+	''' <summary>
+	''' Disposes resources used by the form.
+	''' </summary>
+	''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		If disposing Then
+			If components IsNot Nothing Then
+				components.Dispose()
+			End If
+		End If
+		MyBase.Dispose(disposing)
+	End Sub
+	
+	''' <summary>
+	''' This method is required for Windows Forms designer support.
+	''' Do not change the method contents inside the source code editor. The Forms designer might
+	''' not be able to load this method if it was changed manually.
+	''' </summary>
+	Private Sub InitializeComponent()
+		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+		Me.SourcePath = New System.Windows.Forms.TextBox()
+		Me.label1 = New System.Windows.Forms.Label()
+		Me.SourceFind_BTN = New System.Windows.Forms.Button()
+		Me.edit_keysBTN = New System.Windows.Forms.Button()
+		Me.groupBox1 = New System.Windows.Forms.GroupBox()
+		Me.Tools_BTN = New System.Windows.Forms.Button()
+		Me.label2 = New System.Windows.Forms.Label()
+		Me.buildAPKbtn = New System.Windows.Forms.Button()
+		Me.apkbuilder_output = New System.Windows.Forms.ListBox()
+		Me.button1 = New System.Windows.Forms.Button()
+		Me.label3 = New System.Windows.Forms.Label()
+		Me.statuLBL = New System.Windows.Forms.Label()
+		Me.folderBrowserDialog = New System.Windows.Forms.FolderBrowserDialog()
+		Me.groupBox1.SuspendLayout
+		Me.SuspendLayout
+		'
+		'SourcePath
+		'
+		Me.SourcePath.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+		Me.SourcePath.Location = New System.Drawing.Point(105, 12)
+		Me.SourcePath.Name = "SourcePath"
+		Me.SourcePath.Size = New System.Drawing.Size(411, 24)
+		Me.SourcePath.TabIndex = 0
+		AddHandler Me.SourcePath.TextChanged, AddressOf Me.SourcePathTextChanged
+		'
+		'label1
+		'
+		Me.label1.ForeColor = System.Drawing.Color.Red
+		Me.label1.Location = New System.Drawing.Point(13, 18)
+		Me.label1.Name = "label1"
+		Me.label1.Size = New System.Drawing.Size(100, 15)
+		Me.label1.TabIndex = 1
+		Me.label1.Text = "HTML 5 Project : "
+		'
+		'SourceFind_BTN
+		'
+		Me.SourceFind_BTN.Location = New System.Drawing.Point(522, 13)
+		Me.SourceFind_BTN.Name = "SourceFind_BTN"
+		Me.SourceFind_BTN.Size = New System.Drawing.Size(63, 24)
+		Me.SourceFind_BTN.TabIndex = 2
+		Me.SourceFind_BTN.Text = "Find"
+		Me.SourceFind_BTN.UseVisualStyleBackColor = true
+		AddHandler Me.SourceFind_BTN.Click, AddressOf Me.SourceFind_BTNClick
+		'
+		'edit_keysBTN
+		'
+		Me.edit_keysBTN.ForeColor = System.Drawing.Color.DarkGreen
+		Me.edit_keysBTN.Location = New System.Drawing.Point(28, 35)
+		Me.edit_keysBTN.Name = "edit_keysBTN"
+		Me.edit_keysBTN.Size = New System.Drawing.Size(145, 29)
+		Me.edit_keysBTN.TabIndex = 3
+		Me.edit_keysBTN.Text = "Edit Key Files"
+		Me.edit_keysBTN.UseVisualStyleBackColor = true
+		AddHandler Me.edit_keysBTN.Click, AddressOf Me.Edit_keysBTNClick
+		'
+		'groupBox1
+		'
+		Me.groupBox1.Controls.Add(Me.Tools_BTN)
+		Me.groupBox1.Controls.Add(Me.edit_keysBTN)
+		Me.groupBox1.Controls.Add(Me.label2)
+		Me.groupBox1.ForeColor = System.Drawing.Color.Red
+		Me.groupBox1.Location = New System.Drawing.Point(13, 60)
+		Me.groupBox1.Name = "groupBox1"
+		Me.groupBox1.Size = New System.Drawing.Size(572, 92)
+		Me.groupBox1.TabIndex = 4
+		Me.groupBox1.TabStop = false
+		Me.groupBox1.Text = "Settings"
+		'
+		'Tools_BTN
+		'
+		Me.Tools_BTN.ForeColor = System.Drawing.Color.DarkGreen
+		Me.Tools_BTN.Location = New System.Drawing.Point(396, 35)
+		Me.Tools_BTN.Name = "Tools_BTN"
+		Me.Tools_BTN.Size = New System.Drawing.Size(145, 29)
+		Me.Tools_BTN.TabIndex = 4
+		Me.Tools_BTN.Text = "Tools"
+		Me.Tools_BTN.UseVisualStyleBackColor = true
+		AddHandler Me.Tools_BTN.Click, AddressOf Me.Tools_BTNClick
+		'
+		'label2
+		'
+		Me.label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+		Me.label2.ForeColor = System.Drawing.Color.Red
+		Me.label2.Location = New System.Drawing.Point(179, 37)
+		Me.label2.Name = "label2"
+		Me.label2.Size = New System.Drawing.Size(289, 32)
+		Me.label2.TabIndex = 5
+		Me.label2.Text = "Default Files:"&Global.Microsoft.VisualBasic.ChrW(13)&Global.Microsoft.VisualBasic.ChrW(10)&"> testkey.pk8, testkey.x509.pem"
+		'
+		'buildAPKbtn
+		'
+		Me.buildAPKbtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+		Me.buildAPKbtn.Location = New System.Drawing.Point(13, 168)
+		Me.buildAPKbtn.Name = "buildAPKbtn"
+		Me.buildAPKbtn.Size = New System.Drawing.Size(286, 70)
+		Me.buildAPKbtn.TabIndex = 5
+		Me.buildAPKbtn.Text = "Build APK"
+		Me.buildAPKbtn.UseVisualStyleBackColor = true
+		AddHandler Me.buildAPKbtn.Click, AddressOf Me.BuildAPKbtnClick
+		'
+		'apkbuilder_output
+		'
+		Me.apkbuilder_output.FormattingEnabled = true
+		Me.apkbuilder_output.Location = New System.Drawing.Point(13, 300)
+		Me.apkbuilder_output.Name = "apkbuilder_output"
+		Me.apkbuilder_output.Size = New System.Drawing.Size(572, 160)
+		Me.apkbuilder_output.TabIndex = 6
+		'
+		'button1
+		'
+		Me.button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+		Me.button1.Location = New System.Drawing.Point(299, 168)
+		Me.button1.Name = "button1"
+		Me.button1.Size = New System.Drawing.Size(286, 70)
+		Me.button1.TabIndex = 7
+		Me.button1.Text = "APK Folder"
+		Me.button1.UseVisualStyleBackColor = true
+		AddHandler Me.button1.Click, AddressOf Me.Button1Click
+		'
+		'label3
+		'
+		Me.label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+		Me.label3.ForeColor = System.Drawing.Color.Red
+		Me.label3.Location = New System.Drawing.Point(13, 257)
+		Me.label3.Name = "label3"
+		Me.label3.Size = New System.Drawing.Size(64, 18)
+		Me.label3.TabIndex = 6
+		Me.label3.Text = "Status : "
+		'
+		'statuLBL
+		'
+		Me.statuLBL.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+		Me.statuLBL.ForeColor = System.Drawing.Color.Red
+		Me.statuLBL.Location = New System.Drawing.Point(70, 257)
+		Me.statuLBL.Name = "statuLBL"
+		Me.statuLBL.Size = New System.Drawing.Size(515, 18)
+		Me.statuLBL.TabIndex = 8
+		Me.statuLBL.Text = "N/A"
+		'
+		'MainForm
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+		Me.ClientSize = New System.Drawing.Size(598, 470)
+		Me.Controls.Add(Me.statuLBL)
+		Me.Controls.Add(Me.label3)
+		Me.Controls.Add(Me.button1)
+		Me.Controls.Add(Me.apkbuilder_output)
+		Me.Controls.Add(Me.buildAPKbtn)
+		Me.Controls.Add(Me.groupBox1)
+		Me.Controls.Add(Me.SourceFind_BTN)
+		Me.Controls.Add(Me.SourcePath)
+		Me.Controls.Add(Me.label1)
+		Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+		Me.Icon = CType(resources.GetObject("$this.Icon"),System.Drawing.Icon)
+		Me.Name = "MainForm"
+		Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+		Me.Text = "HTML5 to APK GUI"
+		Me.groupBox1.ResumeLayout(false)
+		Me.ResumeLayout(false)
+		Me.PerformLayout
+	End Sub
+	Private folderBrowserDialog As System.Windows.Forms.FolderBrowserDialog
+	Private statuLBL As System.Windows.Forms.Label
+	Private label3 As System.Windows.Forms.Label
+	Private button1 As System.Windows.Forms.Button
+	Private apkbuilder_output As System.Windows.Forms.ListBox
+	Private buildAPKbtn As System.Windows.Forms.Button
+	Private Tools_BTN As System.Windows.Forms.Button
+	Private label2 As System.Windows.Forms.Label
+	Private groupBox1 As System.Windows.Forms.GroupBox
+	Private edit_keysBTN As System.Windows.Forms.Button
+	Private SourceFind_BTN As System.Windows.Forms.Button
+	Private label1 As System.Windows.Forms.Label
+	Private SourcePath As System.Windows.Forms.TextBox
+End Class
---
+++ MainForm.vb
@@ -0,0 +1,112 @@
+﻿'
+' Created by SharpDevelop.
+' User: IP-Man
+' Date: 1/9/2017
+' Time: 1:51 PM
+' 
+Imports System.IO
+Public Partial Class MainForm
+	Public Sub New()
+		' The Me.InitializeComponent call is required for Windows Forms designer support.
+		Me.InitializeComponent()
+		
+		'
+		' TODO : Add constructor code after InitializeComponents
+		'		
+		
+	End Sub
+		Delegate Sub UpdateTextBoxDelg(ByVal text As String)
+		Public myDelegate As UpdateTextBoxDelg = New UpdateTextBoxDelg(AddressOf UpdateApkBuilderOutput)
+		
+		Public Sub UpdateApkBuilderOutput(ByVal text As String) 'This will update the listbox
+			Dim line As String	= text
+			apkbuilder_output.Items.Add(text)
+			apkbuilder_output.TopIndex = apkbuilder_output.Items.Count - 1	
+			If line.Contains("Compressing") Then statuLBL.Text = "Compressing using 7z."
+			If line.Contains("Extracting") Then statuLBL.Text = "Extracting Files."
+			If line.Contains("You have not got java") Then statuLBL.Text = "Java not found!. Please install Java."
+			If line.Contains("sign apk") Then 
+				statuLBL.Text = "APK has been created!."
+				Process.Start("explorer.exe", "apk")
+			End If
+    End Sub
+
+    Public Sub proc_OutputDataReceived(ByVal sender As Object, ByVal e As DataReceivedEventArgs)
+        On Error Resume Next
+        If Me.InvokeRequired = True Then
+            Me.Invoke(myDelegate, e.Data)
+        Else
+             UpdateApkBuilderOutput(e.Data)
+        End If
+
+    End Sub
+    
+    Private Sub BuildAPK()
+        Dim Proc = New Process()
+		
+		Proc.StartInfo.FileName = "go.bat"
+        Proc.StartInfo.RedirectStandardOutput = True
+        Proc.StartInfo.RedirectStandardError = True
+        Proc.EnableRaisingEvents = True
+        Application.DoEvents()
+        Proc.StartInfo.CreateNoWindow = True
+        Proc.StartInfo.UseShellExecute = False
+
+        AddHandler Proc.ErrorDataReceived, AddressOf proc_OutputDataReceived
+        AddHandler Proc.OutputDataReceived, AddressOf proc_OutputDataReceived
+        Proc.Start()
+        Proc.BeginErrorReadLine()
+        Proc.BeginOutputReadLine()
+        'Proc.WaitForExit()
+        End Sub 
+	
+	Sub BuildAPKbtnClick(sender As Object, e As EventArgs)
+		apkbuilder_output.Items.Clear
+		Call BuildAPK
+	End Sub
+	
+	Sub SourcePathTextChanged(sender As Object, e As EventArgs)
+
+	End Sub
+	
+	
+	Sub MoveAllItems(ByVal fromPath As String, ByVal toPath As String)
+		''Create the target directory if necessary
+		Dim toPathInfo = New DirectoryInfo(toPath)
+		If (Not toPathInfo.Exists) Then
+			toPathInfo.Create()
+		End If
+		Dim fromPathInfo = New DirectoryInfo(fromPath)
+		''move all files
+		For Each file As FileInfo In fromPathInfo.GetFiles()
+			file.CopyTo(Path.Combine(toPath, file.Name))
+			
+		Next
+		
+		''move all folders
+		For Each dir As DirectoryInfo In fromPathInfo.GetDirectories()	
+			dir.MoveTo(Path.Combine(toPath, dir.Name))
+		Next
+		
+	End Sub
+	
+	Sub SourceFind_BTNClick(sender As Object, e As EventArgs)
+		folderBrowserDialog.ShowDialog
+		SourcePath.Text=folderBrowserDialog.SelectedPath
+		System.IO.Directory.Delete("./html5",true)
+		System.IO.Directory.CreateDirectory("./html5")
+		MoveAllItems(SourcePath.Text,"./html5")
+	End Sub
+	
+	Sub Edit_keysBTNClick(sender As Object, e As EventArgs)
+		Process.Start("explorer.exe", "key")
+	End Sub
+	
+	Sub Tools_BTNClick(sender As Object, e As EventArgs)
+		Process.Start("explorer.exe", "tools")
+	End Sub
+	
+	Sub Button1Click(sender As Object, e As EventArgs)
+		Process.Start("explorer.exe", "apk")
+	End Sub
+End Class
